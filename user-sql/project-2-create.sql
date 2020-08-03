f36f48cb16f31e7f439e6f6fb26c98525fce1fc4
Your Name <you@example.com>
2020-08-03 10:18:35 -0400

    schema!

--- user-sql/project-2-create.sql
+++ user-sql/project-2-create.sql
@@ -1,11 +1,10 @@
-create schema project_2;
-set schema 'project_2';
+set database "LOTR-mirco";
+create schema project_2_user_service;
+set schema 'project_2_user_service';
 
 --drop table users;
---drop table locations;
 --drop table users_locations;
---drop table location_images;
---drop table locations_location_images;
+
 
 create table users(
 	"user_id" serial primary key,
@@ -14,13 +13,22 @@ create table users(
 	"first_name" text not null,
 	"last_name" text,
 	"affiliation" text not null,
-	"places_visited" int, 
+	"places_visited" int, -- = COUNT(location_id) FROM user_locations WHERE users.user_id = user_locations.user_id,
 	"address" text,
 	"email" text not null,
 	"role" text not null,
 	"image" text
 );
 
+
+insert into users ("username", "password", "first_name", "last_name", "affiliation", "places_visited", "address", "email", "role", "image")
+	values ('Mithrandir', 'YouShallNotPass', 'Gandalf', null, 'Fellowship', 1233, null, 'shadofaxTheFast@email.com', 'Admin', null),
+		   ('RingBearer', 'myPrecious', 'Frodo', 'Baggins', 'Fellowship', 15, 'The Shire', 'frodoUnderhill@email.com', 'User', null),
+		   ('SamIAm', 'password', 'Samwise', 'Gamgee', 'Fellowship', 15, 'The Shire', 'potatoes4life@email.com', 'User', null),
+		   ('Strider', 'Actually87', 'Aragron II', 'Elessar Telcontar', 'Fellowship', 207, 'Gondor', 'Heir2Isildur@email.com', 'User', null),
+		   ('Dernhelm', 'IAmNoMan', 'Eowyn', null, 'Rohan', 27, 'Rohan', 'shieldMaiden@email.com', 'User', null),
+		   ('NotCountDooku', 'WiseWhiteWizard', 'Saruman', null, 'Sauron', 897, 'Isengard', 'sharkley@email.com', 'User', null);
+		   
 
 	
 
