47c5c6054978381a311f14f6cce3d2ec4c14c17b
Your Name <you@example.com>
2020-08-06 03:18:42 -0400

    jest with logger

--- src/middleware/authentication-middleware.ts
+++ src/middleware/authentication-middleware.ts
@@ -3,7 +3,7 @@ import { logger } from "../utilities/loggers";
 
 export  function authenticationMiddleware (req:any, res:Response, next:NextFunction){
     if (!req.user){
-        res.status(401).send('Please login') //could be a custom error
+        res.status(401).send('Please Login') //could be a custom error
     } else {
         console.log(`User ${req.user.username} has a role of ${req.user.role}`);
         logger.debug(`user ${req.user.username} has a role of ${req.user.role}`);
