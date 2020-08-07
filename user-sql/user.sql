565a3ff4fb72293e8472d06964553ec778ce62b3
RchlRiedel <rchlriedel27@gmail.com>
2020-08-07 18:59:19 -0400

    updated sql script

--- user-sql/user.sql
+++ user-sql/user.sql
@@ -1,4 +1,4 @@
-set database "LOTR-mirco";
+--set database 'LOTR-mirco';
 create schema project_2_user_service;
 set schema 'project_2_user_service';
 
@@ -20,11 +20,20 @@ create table users(
 	"image" text
 );
 
-
-insert into users ("username", "password", "first_name", "last_name", "affiliation", "places_visited", "address", "email", "role", "image")
-	values ('Mithrandir', 'YouShallNotPass', 'Gandalf', null, 'Fellowship', 1233, null, 'shadofaxTheFast@email.com', 'Admin', null),
-		   ('RingBearer', 'myPrecious', 'Frodo', 'Baggins', 'Fellowship', 15, 'The Shire', 'frodoUnderhill@email.com', 'User', null),
-		   ('SamIAm', 'password', 'Samwise', 'Gamgee', 'Fellowship', 15, 'The Shire', 'potatoes4life@email.com', 'User', null),
-		   ('Strider', 'Actually87', 'Aragron II', 'Elessar Telcontar', 'Fellowship', 207, 'Gondor', 'Heir2Isildur@email.com', 'User', null),
-		   ('Dernhelm', 'IAmNoMan', 'Eowyn', null, 'Rohan', 27, 'Rohan', 'shieldMaiden@email.com', 'User', null),
-		   ('NotCountDooku', 'WiseWhiteWizard', 'Saruman', null, 'Sauron', 897, 'Isengard', 'sharkley@email.com', 'User', null);
+insert into users ("username", "password", "first_name", "last_name", "affiliation", "address", "email", "role", "image")
+	values ('Mithrandir', 'YouShallNotPass', 'Gandalf', null, 'Fellowship', null, 'shadofaxTheFast@email.com', 'Admin', null),
+		   ('RingBearer', 'myPrecious', 'Frodo', 'Baggins', 'Fellowship', 'The Shire', 'frodoUnderhill@email.com', 'User', null),
+		   ('SamIAm', 'password', 'Samwise', 'Gamgee', 'Fellowship', 'The Shire', 'potatoes4life@email.com', 'User', null),
+           ('Strider', 'Actually87', 'Aragron II', 'Elessar Telcontar', 'Fellowship', 'Gondor', 'Heir2Isildur@email.com', 'User', null),
+           ('GoldenGimli','ThatStillOnlyCountsAs1', 'Gimli', null, 'Fellowship', 'Erebor', 'lockBearer@email.com', 'User', null),
+           ('CaptainOfTheWhiteTower', 'NearamirFaramir', 'Boromir', null, 'Fellowship', 'Gondor', 'sterwardPrince@email.com', 'User', null),
+           ('LorealLegolas', 'BecauseYouAreWorthIt', 'Legolas', 'Greenleaf', 'Fellowship', 'Mirkwood', 'EndlessQuiver@email.com', 'User', null),
+           ('Evenstar', 'Queen2Be', 'Arwen', null, 'Fellowship', 'Riverdell', 'comeANDclaimHIM@email.com', 'User', null),
+           ('Galadrielf', 'LadyofLight', 'Galadriel', null, 'Fellowship', 'Lothlorien', 'greatAndTerrible@email.com', 'Admin', null),
+		   ('Dernhelm', 'IAmNoMan', 'Eowyn', null, 'Rohirrim', 'Rohan', 'shieldMaiden@email.com', 'User', null),
+		   ('NotCountDooku', 'WiseWhiteWizard', 'Saruman', null, 'Sauron', 'Isengard', 'sharkley@email.com', 'User', null),
+           ('MerryMerry', 'BrandybuckBoi', 'Meriadoc', 'Brandybuck', 'Fellowship', 'The Shire','tallerThanPippin@email.com', 'User', null),
+           ('FoolOfATook', '00psMyBad', 'Peregrin', 'Took', 'Fellowship', 'The Shire', 'tallerThanMerry@email.com', 'User', null),
+           ('WitchKing', 'ThisKing', 'Forgotten', null, 'Sauron', 'Angmar', 'noManCanBeatMe@email.com', 'User', null),
+           ('SnakeySnake', 'notASpy', 'Grima', 'Wormtongue', 'Saruman', 'Rohan', 'everyoneHatesMe@emai.com', 'User', null);
+--select * from users;
